8dad7f97260a5bf4e4d1379601305692cd81bfee
eiuThanhDuy <duynguyen120504@gmail.com>
2025-05-26 22:27:05 +0700

    Model of class
    This is not the final for class description.

---
+++ staffsmanagement/src/main/java/com/cse305/Models/Manager.java
@@ -0,0 +1,6 @@
+package com.cse305.Models;
+
+public class Student {
+    public int id;
+    public String name;
+}
--- staffsmanagement/src/main/java/com/cse305/PrimaryController.java
+++ staffsmanagement/src/main/java/com/cse305/PrimaryController.java
@@ -1,6 +1,10 @@
 package com.cse305;
 
 import java.io.IOException;
+
+import com.cse305.Models.Student;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -8,5 +12,14 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+    public void show(ActionEvent e) {
+        System.out.println("cccccccccccc");
+    }
+    public void showw2(ActionEvent e) {
+        Student student = new Student();
+        student.name = "duy123";
+        student.id = 1113;
+        System.out.println(student.name + student.id);
     }
 }
